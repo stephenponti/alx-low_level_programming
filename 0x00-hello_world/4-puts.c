f8fcc9d492b1a0b108d4e982243a34645a998f1b
stephenponti <stephenponti@gmail.com>
2022-12-08 12:38:26 -0800

    hit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+*/                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	        puts("\"Programming is like building a multi lingual puzzle);                                                            
+			        return (0);
+}
