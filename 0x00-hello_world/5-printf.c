f2d10ab28874be0b75620300ea7b1975eaa58dfb
stephenponti <stephenponti@gmail.com>
2022-12-09 00:42:09 -0800

    git

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
+ * main - Entry
  * Return: Always 0
  */
-int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
 }
