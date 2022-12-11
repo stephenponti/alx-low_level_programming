e87ba2471609d24ff5c2eb17fdc886e1ac79e5a3
stephenponti <stephenponti@gmail.com>
2022-12-11 11:38:29 -0800

    git

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,19 +10,21 @@
 int main(void)
 {
 	int n;
-srand(time(0));
+
+	srand(time(0));
+
 	n = rand(0) - RAND_MAX / 2;
 	if (n > 0)
-	(
+{
 	 printf("%d is positive\n", n);
-	)
+}
 	else if (n == 0)
-	(
+{
 	 printf("%d is zero\n", n);
-	)
+{
 	else
-	(
+{
 	 printf("%d is negative\n", n);
-	)
+}
 	return (0);
 }
