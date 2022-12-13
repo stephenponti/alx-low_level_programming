5ce3c31fddce7e38e2ac5ca3804a230468dc27ac
stephenponti <stephenponti@gmail.com>
2022-12-13 08:27:07 -0800

    git

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers of base 16.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+	char c;
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+		putchar(d + '0');
+		d++;
+		}
+	while
+		(c <= 'f');
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
